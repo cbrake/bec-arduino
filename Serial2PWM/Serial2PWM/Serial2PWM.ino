f327d0d002f9b08a8733bc22933dcc04a3f3eda5
Cliff Brake <cbrake@bec-systems.com>
2013-10-18 12:54:14 -0400

    update Serial2PWM

--- Serial2PWM/Serial2PWM/Serial2PWM.ino
+++ Serial2PWM/Serial2PWM/Serial2PWM.ino
@@ -1,27 +1,20 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
+  Reads a binary byte on serial line and outputs
+  value to PWM channel
  */
  
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 13;
-
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
-  analogWrite(3, 128);
+  analogWrite(3, 0);
   Serial.begin(115200);
 }
 
+int incomingByte = 0;
+
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);               // wait for a second
-  Serial.print("hi ");
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    analogWrite(3, incomingByte);
+  }
 }
