9a051abdf2ef4d125bcc0e8f41d579def51e4a81
Cliff Brake <cbrake@bec-systems.com>
2017-08-15 09:25:12 -0400

    test

---
+++ esp8266/test/test.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(10);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("hi there cliff");
+  delay(1000);
+}
